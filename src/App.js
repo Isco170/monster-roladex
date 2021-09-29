a8f53c7cbf8e4aea4ad2ff5ffbe130735b51abf3
Isco170 <franciscodamana@gmail.com>
2021-09-29 21:05:12 +0200

    SearchField

--- src/App.js
+++ src/App.js
@@ -21,10 +21,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <input type="search" placeholder="search monsters" onChange={ e => {
-          this.setState({ searchField: e.target.value});
-          console.log(this.state);
-          }}
+        <input type="search" placeholder="search monsters" onChange={ e =>
+          this.setState({ searchField: e.target.value}}
         />
         <CardList monsters={this.state.monsters} />
     </div>
