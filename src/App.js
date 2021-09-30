7446a7f8a28a2b9aa48b49d1ca4636ddb4941091
Isco170 <franciscodamana@gmail.com>
2021-09-30 22:06:25 +0200

    Class Methods and Arrow Functions

--- src/App.js
+++ src/App.js
@@ -13,12 +13,17 @@ class App extends Component {
       monsters: [],
       searchField: ''
     };
+
   }
 
   componentDidMount(){
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(users => this.setState({ monsters: users }));
+  }
+
+  handleChange = (e) =>{
+    this.setState({ searchField: e.target.value})
   }
 
   render() {
@@ -30,7 +35,7 @@ class App extends Component {
       <div className="App">
         <SearchBox
           placeholder='search monsters'
-          handleChange={e => this.setState({ searchField: e.target.value})}
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters} />
     </div>
